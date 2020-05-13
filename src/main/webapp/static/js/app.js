216bc5e164e5649dd640514dc5c73f19b6e8e055
Tajti Bendegúz <bendeguz9ctajti@gmail.com>
2020-05-13 17:51:14 +0200

    add product page animation

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -1,10 +1,68 @@
 new fullpage('#fullpage', {
     autoScrolling: true,
-    navigation: true
+    navigation: true,
+    afterRender: () => {
+        TweenMax.staggerFrom('.welcomeMessage', .4, {
+            opacity: 0,
+            delay: .2,
+            y: -40,
+            ease: Expo.ease
+        })
+    },
+    onLeave: (origin, destination, direction) => {
+        if (destination.index > 0 && destination.index < 8) {
+            const currentSection = destination.item;
+            if (destination.index % 2 === 0) {
+                productLeftAnimation(currentSection)
+            } else {
+                productRightAnimation(currentSection)
+            }
+        }
+    }
 });
 
-const navRight = document.querySelector('#fp-nav');
-console.log(navRight.children)
+const productLeftAnimation = (section) => {
+    if (!section.classList.contains('animated')) {
+        const pageContent = section.childNodes[0].childNodes[1].childNodes[1];
+        const productImage = pageContent.children[0];
+        const productText = pageContent.children[1];
+        TweenMax.staggerFrom(productText, 0.4, {
+            opacity: 0,
+            delay: 0.65,
+            x: 40,
+            ease: Expo.ease
+        });
+        TweenMax.staggerFrom(productImage, 0.45, {
+            opacity: 0,
+            delay: 0.75,
+            x: -40,
+            ease: Expo.ease
+        });
+        section.classList.add('animated')
+    }
+}
+
+const productRightAnimation = (section) => {
+    if (!section.classList.contains('animated')) {
+        const pageContent = section.childNodes[0].childNodes[1].childNodes[1];
+        const productImage = pageContent.children[1];
+        const productText = pageContent.children[0];
+        TweenMax.staggerFrom(productImage, 0.45, {
+            opacity: 0,
+            delay: 0.75,
+            x: 40,
+            ease: Expo.ease
+        });
+        TweenMax.staggerFrom(productText, 0.4, {
+            opacity: 0,
+            delay: 0.65,
+            x: -40,
+            ease: Expo.ease
+        });
+        section.classList.add('animated')
+    }
+}
+
 
 let buttons = document.querySelectorAll(".cartButton");
 
