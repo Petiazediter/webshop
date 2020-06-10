46f4b6977facbd65c527836f7872adcd9c7c4b0d
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-10 14:47:56 +0200

    adds UserDao interface

--- src/main/java/com/codecool/shop/dao/implementation/UserDao.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDao.java
@@ -2,9 +2,13 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.model.User;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserDao {
     void add(User user);
     User find(int userId);
+    void remove(User user);
+
+    List<User> getAll();
 }
