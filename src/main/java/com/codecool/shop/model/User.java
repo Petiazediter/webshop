498b6826e7180c11ad788595b4e9dbe5058dc339
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-10 14:30:02 +0200

    defines User class model's attributes, getters and setters

--- src/main/java/com/codecool/shop/model/User.java
+++ src/main/java/com/codecool/shop/model/User.java
@@ -1,4 +1,39 @@
 package com.codecool.shop.model;
 
+import java.util.UUID;
+
 public class User {
+    private UUID userId = UUID.randomUUID();
+    private String name;
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
